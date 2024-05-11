0dea4dd5a0e4447feeb488a63b2832e2d1d2e600
faiazsm <faiazsm@gmail.com>
2024-05-11 12:02:42 +0530

    Assignment2_CBT

---
+++ src/test/java/A02CBT/crossBrowserTestAssignment.java
@@ -0,0 +1,14 @@
+package A02CBT;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class crossBrowserTestAssignment {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
